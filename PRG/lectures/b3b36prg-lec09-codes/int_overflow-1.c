7c803fba22de545427125892542c2e274d716ae1
Marchell0o0 <mark.o.horpynych@gmail.com>
2023-04-30 19:23:21 +0200

    Remove lecutres code

--- PRG/lectures/b3b36prg-lec09-codes/int_overflow-1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-   for (int i = 2147483640; i >= 0; ++i) {
-      printf("%i %x\n", i, i);
-   }
-   return 0;
-}
--- PRG/lectures/b3b36prg-lec09-codes/pipes/create_pipes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pipe=/tmp/prg-lec09.pipe
-
-rm -rf $pipe
-mkfifo $pipe
